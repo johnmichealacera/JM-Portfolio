340868a9e2e62072fea1dc60e4acb6d70f3f6f7c
John Acera <john.acera@sportality.se>
2022-04-23 17:54:11 +0800

    added personal project shooting bird

--- src/components/Project.vue
+++ src/components/Project.vue
@@ -2,34 +2,28 @@
 <div class="py-10 px-2">
   <h1 class="text-center text-2xl font-medium">Checkout my Personal Projects</h1>
   <hr class="m-6">
-  <div class="bg-project w-1/2 h-96 group" @mouseover="showModal" @mouseleave="hideModal">
-    <app-modal v-if="modalVisible"></app-modal>
+  <div class="flex space-x-4">
+    <div class="bg-project w-1/2 h-96 group">
+    <app-modal class="opacity-0 hover:opacity-100"></app-modal>
+    </div>
+    <div class="bg-shooting-bird w-1/2 h-96 group">
+      <shooting-bird-project-modal class="opacity-0 hover:opacity-100"></shooting-bird-project-modal>
+    </div>
   </div>
 </div>
 </template>
 
 <script lang='ts'>
 import AppModal from './AppModal.vue';
-import { ref } from 'vue';
+import ShootingBirdProjectModal from './ShootingBirdProjectModal.vue';
 
-const modalVisible = ref(false);
 export default {
   name: "Project",
   components: {
     AppModal,
+    ShootingBirdProjectModal,
   },
   setup() {
-    const showModal = () => {
-      modalVisible.value = true;
-    };
-    const hideModal = () => {
-      modalVisible.value = false;
-    };
-    return {
-      showModal,
-      modalVisible,
-      hideModal,
-    }
   }
 }
 </script>
@@ -37,6 +31,11 @@ export default {
 <style scoped>
   .bg-project {
     background-image: url('../../public/todo.png');
+    background-repeat: no-repeat;
+    background-size: 100% 100%;;
+  }
+  .bg-shooting-bird {
+    background-image: url('../../public/shoot-birds.png');
     background-repeat: no-repeat;
     background-size: 100% 100%;;
   }
---
+++ src/components/ShootingBirdProjectModal.vue
@@ -0,0 +1,12 @@
+<template>
+    <ul class="h-full flex flex-col justify-center items-center bg-gray-900 bg-opacity-75 text-white space-y-5">
+        <li class="text-2xl"><strong>Project 2 </strong>Shooting Bird Game</li>
+        <li class="text-sm">A game where you shoot down birds using your mouse.</li>
+        <li><a class="bg-blue-600 m-2 p-2 rounded text-white text-opacity-60 hover:text-opacity-90 text-sm" href="https://shooting-birds-game.herokuapp.com//" target="_blank">SEE PROJECT</a></li>
+    </ul>
+</template>
+<script>
+  export default {
+    name: 'app-modal',
+  };
+</script>
