00b443d4167df10036141c4fc8962ed322004b6e
John Acera <john.acera@sportality.se>
2023-01-31 13:52:15 +0800

    added additional story and update github link

--- src/components/AboutMe.vue
+++ src/components/AboutMe.vue
@@ -5,7 +5,10 @@
       <hr class="m-6">
       <p class="text-justify leading-loose py-5">I am a graduate of Bachelor of Science in Information Technology way back 2015. After graduating, i worked on two jobs unrelated to my course. First is working at the Accounting Department in the government and then recently was as a Customer Service Representative.
 
-    I decided to quit my job as a CSR last April 2020 to pursue my passion and profession as a Web Developer. I then started relearning stuffs and is very motivated to work hard on improving my skills as well as to be able to land a job as a programmer.</p>
+    I decided to quit my job as a CSR last April 2020 to pursue my passion and profession as a Web Developer. I then started relearning stuffs and is very motivated to work hard on improving my skills as well as to be able to land a job as a programmer.
+  
+  I worked as freelancer for some time and finally got my first IT corporate job on January 2021.
+Right now, I am working for 2 years as a Full Stack Developer with some knowledge of DevOps.</p>
   </div>
 </div>
 </template>
