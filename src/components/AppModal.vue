29d86bc1b7243119bdf8f4bdcb6af9e7f6d9efc8
John Acera <john.acera@sportality.se>
2023-01-31 13:34:50 +0800

    added project url env variables

--- src/components/AppModal.vue
+++ src/components/AppModal.vue
@@ -2,11 +2,19 @@
     <ul class="h-full flex flex-col justify-center items-center bg-gray-900 bg-opacity-75 text-white space-y-5">
         <li class="text-2xl"><strong>Project 1 </strong>To Do App</li>
         <li class="text-sm">A simple to do list using pure javascript, DOM and events.</li>
-        <li><a class="bg-blue-600 m-2 p-2 rounded text-white text-opacity-60 hover:text-opacity-90 text-sm" href="https://jm-todoapp.herokuapp.com/" target="_blank">SEE PROJECT</a></li>
+        <li><a class="bg-blue-600 m-2 p-2 rounded text-white text-opacity-60 hover:text-opacity-90 text-sm" :href=todoAppUrl target="_blank">SEE PROJECT</a></li>
     </ul>
 </template>
 <script>
+import { computed } from 'vue';
   export default {
     name: 'app-modal',
+    setup(){
+    const todoAppUrl = computed(() => process.env.VUE_APP_TODO);
+
+    return {
+      todoAppUrl
+    }
+  }
   };
 </script>
--- src/components/ShootingBirdProjectModal.vue
+++ src/components/ShootingBirdProjectModal.vue
@@ -2,11 +2,19 @@
     <ul class="h-full flex flex-col justify-center items-center bg-gray-900 bg-opacity-75 text-white space-y-5">
         <li class="text-2xl"><strong>Project 2 </strong>Shooting Bird Game</li>
         <li class="text-sm">A game where you shoot down birds using your mouse.</li>
-        <li><a class="bg-blue-600 m-2 p-2 rounded text-white text-opacity-60 hover:text-opacity-90 text-sm" href="https://shooting-birds-game.herokuapp.com//" target="_blank">SEE PROJECT</a></li>
+        <li><a class="bg-blue-600 m-2 p-2 rounded text-white text-opacity-60 hover:text-opacity-90 text-sm" :href=shootingBirUrl target="_blank">SEE PROJECT</a></li>
     </ul>
 </template>
 <script>
+import { computed } from 'vue';
   export default {
     name: 'app-modal',
+    setup() {
+    const shootingBirUrl = computed(() => process.env.VUE_APP_SHOOTING_BIRDS);
+
+    return {
+      shootingBirUrl
+      }
+    }
   };
 </script>
