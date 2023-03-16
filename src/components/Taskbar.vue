2665d9582cf29531a04c9612959d3e6d1bd0575f
John Acera <john.acera@sportality.se>
2023-03-16 14:09:06 +0800

    remove dynamic name value for the home button in taskbar

--- src/components/Taskbar.vue
+++ src/components/Taskbar.vue
@@ -1,7 +1,6 @@
 <template>
   <div class="flex justify-evenly items-center text-xs font-black text-gray-500 p-10">
-    <loader :isLoading="isLoading"/>
-    <router-link to="/" class="hover:bg-green-700 uppercase text-white text-xl name-link rounded-full p-2 mr-2" v-if="!isLoading">{{ fullName }}</router-link>
+    <router-link to="/" class="hover:bg-green-700 uppercase text-white text-xl name-link rounded-full p-2 mr-2" v-if="!isLoading">Home</router-link>
     <ul class="flex space-x-4">
       <li class=""><router-link to="/" class="hover:underline sm:p-6 uppercase">Home</router-link></li>
       <li class=""><router-link to="/about" class="hover:underline sm:p-6 uppercase">About Me</router-link></li>
@@ -12,30 +11,8 @@
 </template>
 
 <script>
-import Loader from './commons/Loader.vue';
-import { onMounted, ref } from '@vue/runtime-core';
-import { usePortfolioStore } from '@/store/pinia/portfolio';
 export default {
   name: 'Taskbar',
-  components: {
-    Loader,
-  },
-  setup() {
-    const portfolioStore = usePortfolioStore();
-    const fullName = ref('');
-    const isLoading = ref(false);
-
-    onMounted(async () => {
-      isLoading.value = true;
-      await portfolioStore.fetchIntroductionsData(process.env.VUE_APP_USER_ID);
-      fullName.value = portfolioStore.introductionData?.fullName;
-      isLoading.value = false;
-    });
-
-    return {
-      fullName,
-    }
-  }
 }
 </script>
 
