d6f6efe7b6a3a82e0eaae974cfdfb8c32644c27b
John Acera <john.acera@sportality.se>
2023-05-06 22:19:32 +0800

    used login component in view directory

--- src/router/index.js
+++ src/router/index.js
@@ -44,7 +44,7 @@ const routes = [
   {
     path: '/login',
     name: 'Login',
-    component: () => import('@/components/Login.vue'),
+    component: () => import('@/views/Login.vue'),
     meta: {
       title: 'Login',
     },
