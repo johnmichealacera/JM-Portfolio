077b99dd0520333bb65a714f0845bf274be51d20
JM Acera <john.acera@sportality.se>
2024-04-10 23:57:38 +0800

    integrate graphql call in portfolio store

--- src/store/pinia/portfolio.js
+++ src/store/pinia/portfolio.js
@@ -41,82 +41,310 @@ export const usePortfolioStore = defineStore({
     },
   },
   actions: {
-    async fetchIntroductionsData(userId) {
-      if (typeof userId === 'undefined') {
-        return;
-      }
+    // Graphql api equivalent from rust api
+    async fetchIntroductionsData() {
+      const query = `
+        {
+          users {
+            fullName
+          }
+          personals {
+            jobDescription
+          }
+          skillsOverview  {
+            title
+            icon
+          }
+        }
+      `;
+    
       try {
-        const introductions = await axios.get(`${process.env.VUE_APP_PORTFOLIO_BACKEND}/introductions/${userId}`,);
-        this.introductions = introductions?.data;
+        const data = await axios.post(
+          `${process.env.VUE_APP_PORTFOLIO_BACKEND}/graphql`,
+          query,
+          {
+            headers: {
+              'Content-Type': 'application/graphql',
+            },
+          }
+        );
+        const userData = data?.data?.data.users?.[0];
+        const personalData = data?.data?.data.personals?.[0];
+        const skillsOverviewData = data?.data?.data.skillsOverview;
+
+        this.introductions = {
+          fullName: userData?.fullName,
+          jobDescription: personalData?.jobDescription,
+          expertise: skillsOverviewData,
+        };
       } catch (error) {
         console.error(error);
       }
     },
-    async fetchSocialMediaData(userId) {
-      if (typeof userId === 'undefined') {
-        return;
-      }
+    async fetchSocialMediaData() {
+      const query = `
+        {
+          socialMedia {
+            socialMediaType
+            url
+          }
+        }
+      `;
+    
       try {
-        const socialMedia = await axios.get(`${process.env.VUE_APP_PORTFOLIO_BACKEND}/social-media/${userId}`);
-        this.socialMedia = socialMedia?.data;
+        const data = await axios.post(
+          `${process.env.VUE_APP_PORTFOLIO_BACKEND}/graphql`,
+          query,
+          {
+            headers: {
+              'Content-Type': 'application/graphql',
+            },
+          }
+        );
+        const rawSocials = data?.data?.data.socialMedia?.map((item) => ({
+          url: item?.url,
+          type: item?.socialMediaType,
+        }));
+        this.socialMedia = rawSocials;
       } catch (error) {
         console.error(error);
       }
     },
-    async fetchProjectsData(userId) {
-      if (typeof userId === 'undefined') {
-        return;
-      }
+    async fetchUserInfo() {
+      const query = `
+        {
+          personals {
+            backgroundUrl
+            jobDescription
+            whyDoThis
+            lifeStory
+          }
+        }
+      `;
+    
       try {
-        const projects = await axios.get(`${process.env.VUE_APP_PORTFOLIO_BACKEND}/projects/${userId}`);
-        this.projects = projects?.data;
+        const data = await axios.post(
+          `${process.env.VUE_APP_PORTFOLIO_BACKEND}/graphql`,
+          query,
+          {
+            headers: {
+              'Content-Type': 'application/graphql',
+            },
+          }
+        );
+        const personalData = data?.data?.data?.personals?.[0];
+        const userInfo = {
+          lifeStory: personalData?.lifeStory,
+          bgUrl: personalData?.backgroundUrl,
+          userWhy: personalData?.whyDoThis,
+        }
+        this.userInfo = userInfo;
       } catch (error) {
         console.error(error);
       }
     },
-    async fetchSkillsData(userId) {
-      if (typeof userId === 'undefined') {
-        return;
-      }
+    async fetchProjectsData() {
+      const query = `
+        {
+          projects {
+            backgroundImage
+            description
+            title
+            url
+          }
+        }
+      `;
+    
       try {
-        const skills = await axios.get(`${process.env.VUE_APP_PORTFOLIO_BACKEND}/skills/${userId}`);
-        this.skills = skills?.data;
+        const data = await axios.post(
+          `${process.env.VUE_APP_PORTFOLIO_BACKEND}/graphql`,
+          query,
+          {
+            headers: {
+              'Content-Type': 'application/graphql',
+            },
+          }
+        );
+        const rawProjectsData = data?.data?.data?.projects;
+        const projectsdata = rawProjectsData?.map((project) => ({
+          imageLink: project?.backgroundImage,
+          title: project?.title,
+          description: project?.description,
+          url: project?.url,
+        }))
+        this.projects = projectsdata;
       } catch (error) {
         console.error(error);
       }
     },
-    async fetchUserInfo(userId) {
-      if (typeof userId === 'undefined') {
-        return;
-      }
+    async fetchSkillsData() {
+      const query = `
+        {
+          skills {
+            name
+            mastery
+            skillType
+          }
+        }
+      `;
+    
       try {
-        const userInfo = await axios.get(`${process.env.VUE_APP_PORTFOLIO_BACKEND}/user-info/${userId}`);
-        this.userInfo = userInfo?.data;
+        const data = await axios.post(
+          `${process.env.VUE_APP_PORTFOLIO_BACKEND}/graphql`,
+          query,
+          {
+            headers: {
+              'Content-Type': 'application/graphql',
+            },
+          }
+        );
+        const rawSkillsData = data?.data?.data?.skills;
+        const skillsdata = rawSkillsData?.map((skill) => ({
+          ...skill,
+          type: skill?.skillType,
+        }))
+        this.skills = skillsdata;
       } catch (error) {
         console.error(error);
       }
     },
-    async fetchUserDetails(userId) {
-      if (typeof userId === 'undefined') {
-        return;
-      }
+    async fetchUserDetails() {
+      const query = `
+        {
+          users {
+            fullName
+            email
+            contactNumber
+            website
+          }
+        }
+      `;
+    
       try {
-        const userDetails= await axios.get(`${process.env.VUE_APP_PORTFOLIO_BACKEND}/user-details/${userId}`);
-        this.userDetails = userDetails?.data;
+        const data = await axios.post(
+          `${process.env.VUE_APP_PORTFOLIO_BACKEND}/graphql`,
+          query,
+          {
+            headers: {
+              'Content-Type': 'application/graphql',
+            },
+          }
+        );
+        const usersData = data?.data?.data?.users?.[0];
+        this.userDetails = usersData;
       } catch (error) {
         console.error(error);
       }
     },
-    async fetchSoftSkills(userId) {
-      if (typeof userId === 'undefined') {
-        return;
-      }
+    async fetchSoftSkills() {
+      const query = `
+        {
+          softSkills {
+            name
+            description
+            icon
+          }
+        }
+      `;
+    
       try {
-        const softSkills= await axios.get(`${process.env.VUE_APP_PORTFOLIO_BACKEND}/soft-skills/${userId}`);
-        this.softSkills = softSkills?.data;
+        const data = await axios.post(
+          `${process.env.VUE_APP_PORTFOLIO_BACKEND}/graphql`,
+          query,
+          {
+            headers: {
+              'Content-Type': 'application/graphql',
+            },
+          }
+        );
+        const rawSoftSkillsData = data?.data?.data?.softSkills;
+        const softSkillsData = rawSoftSkillsData?.map((softSkill) => ({
+          icon: softSkill?.icon,
+          name: softSkill?.name,
+          shortDescription: softSkill?.description,
+        }));
+        this.softSkills = softSkillsData;
       } catch (error) {
         console.error(error);
       }
     },
+    // Rest api equivalent from express api
+    // async fetchIntroductionsData(userId) {
+    //   if (typeof userId === 'undefined') {
+    //     return;
+    //   }
+    //   try {
+    //     const introductions = await axios.get(`${process.env.VUE_APP_PORTFOLIO_BACKEND}/introductions/${userId}`,);
+    //     this.introductions = introductions?.data;
+    //   } catch (error) {
+    //     console.error(error);
+    //   }
+    // },
+    // async fetchSocialMediaData(userId) {
+    //   if (typeof userId === 'undefined') {
+    //     return;
+    //   }
+    //   try {
+    //     const socialMedia = await axios.get(`${process.env.VUE_APP_PORTFOLIO_BACKEND}/social-media/${userId}`);
+    //     this.socialMedia = socialMedia?.data;
+    //   } catch (error) {
+    //     console.error(error);
+    //   }
+    // },
+    // async fetchProjectsData(userId) {
+    //   if (typeof userId === 'undefined') {
+    //     return;
+    //   }
+    //   try {
+    //     const projects = await axios.get(`${process.env.VUE_APP_PORTFOLIO_BACKEND}/projects/${userId}`);
+    //     this.projects = projects?.data;
+    //   } catch (error) {
+    //     console.error(error);
+    //   }
+    // },
+    // async fetchSkillsData(userId) {
+    //   if (typeof userId === 'undefined') {
+    //     return;
+    //   }
+    //   try {
+    //     const skills = await axios.get(`${process.env.VUE_APP_PORTFOLIO_BACKEND}/skills/${userId}`);
+    //     this.skills = skills?.data;
+    //   } catch (error) {
+    //     console.error(error);
+    //   }
+    // },
+    // async fetchUserInfo(userId) {
+    //   if (typeof userId === 'undefined') {
+    //     return;
+    //   }
+    //   try {
+    //     const userInfo = await axios.get(`${process.env.VUE_APP_PORTFOLIO_BACKEND}/user-info/${userId}`);
+    //     this.userInfo = userInfo?.data;
+    //   } catch (error) {
+    //     console.error(error);
+    //   }
+    // },
+    // async fetchUserDetails(userId) {
+    //   if (typeof userId === 'undefined') {
+    //     return;
+    //   }
+    //   try {
+    //     const userDetails= await axios.get(`${process.env.VUE_APP_PORTFOLIO_BACKEND}/user-details/${userId}`);
+    //     this.userDetails = userDetails?.data;
+    //   } catch (error) {
+    //     console.error(error);
+    //   }
+    // },
+    // async fetchSoftSkills(userId) {
+    //   if (typeof userId === 'undefined') {
+    //     return;
+    //   }
+    //   try {
+    //     const softSkills= await axios.get(`${process.env.VUE_APP_PORTFOLIO_BACKEND}/soft-skills/${userId}`);
+    //     this.softSkills = softSkills?.data;
+    //   } catch (error) {
+    //     console.error(error);
+    //   }
+    // },
   },
 });
